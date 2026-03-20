f5acdb15aa397b4c89db33d3fca6d7a56853df0c
vowmoments <vowmoments696@gmail.com>
2026-03-20 19:33:36 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import {
+    defineConfig
+} from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    base: '/',
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
+    server: {
+        historyApiFallback: true,
+    },
+    preview: {
+        historyApiFallback: true,
+    },
+})
